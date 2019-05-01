85d3f31d26ae5e266e69c4f7e99d1ee594d26a6e
Olga Pasynok <baushonok@gmail.com>
2019-05-01 21:37:39 +0300

    Auth (#1)
    * Add authentication
    
    * Add styles, update warning states
    
    * Fixes after review

---
+++ src/components/Auth/index.tsx
@@ -0,0 +1,90 @@
+import React, {
+  Component,
+  SyntheticEvent,
+  FormEvent
+} from 'react';
+import WarningMessage from './WarningMessage';
+import { IS_LOGGED_ON } from './constants';
+import './index.css';
+
+interface IProps {
+  onSuccessLoginHandler: () => void,
+};
+
+interface IState {
+  login: string,
+  password: string,
+  shouldShowWarning: boolean
+};
+
+export default class Auth extends Component<IProps, IState> {
+  state = {
+    login: '',
+    password: '',
+    shouldShowWarning: false
+  };
+
+  login = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { login, password } = this.state;
+
+    if (login === 'login' && password === 'password') {
+      localStorage.setItem(IS_LOGGED_ON, 'true');
+      this.props.onSuccessLoginHandler();
+      return;
+    }
+
+    this.setState({
+      shouldShowWarning: true
+    });
+  }
+  handleChangeLogin = (event: SyntheticEvent<HTMLInputElement>) => {
+    let target = event.target as HTMLInputElement;
+    this.setState({
+      login: target.value,
+      shouldShowWarning: false,
+    });
+  }
+  handleChangePassword = (event: SyntheticEvent<HTMLInputElement>) => {
+    let target = event.target as HTMLInputElement;
+    this.setState({
+      password: target.value,
+      shouldShowWarning: false,
+    });
+  }
+  render() {
+    const { login, password, shouldShowWarning } = this.state;
+
+    return (
+      <form className="auth-form"
+            onSubmit={this.login}>
+        <div className="auth-form__login">
+          <label htmlFor="login">
+            Login
+          </label>
+          <input type="text"
+                 id="login"
+                 name="login"
+                 value={login}
+                 onChange={this.handleChangeLogin} />
+        </div>
+        <div className="auth-form__password">
+          <label htmlFor="password">
+            Password
+          </label>
+          <input type="password"
+                 id="password"
+                 name="password"
+                 value={password}
+                 onChange={this.handleChangePassword} />
+        </div>
+        {shouldShowWarning ? <WarningMessage /> : null}
+        <button type="submit"
+                className="auth-form__button"
+                disabled={shouldShowWarning}>
+          Log in
+        </button>
+      </form>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Auth from './components/Auth';
 import Game from './components/Game';
+import { IS_LOGGED_ON } from './components/Auth/constants';
 import './index.css';
 
 // ========================================
 
+interface IState {
+  isLoggenOn: boolean
+}
+
+class Content extends Component<{}, IState> {
+  state = {
+    isLoggenOn: !!localStorage.getItem(IS_LOGGED_ON),
+  };
+
+  successLoginHandler = () => {
+    this.setState({ isLoggenOn: true });
+  }
+
+  render() {
+    const { isLoggenOn } = this.state;
+
+    return isLoggenOn
+      ? <Game />
+      : <Auth onSuccessLoginHandler={this.successLoginHandler} />;
+  }
+}
+
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error('Root element does not exist');
+}
+
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+  <Content />,
+  container
 );
